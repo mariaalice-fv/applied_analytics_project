654a5bdf357a4d93298a3c025b04ea03b87f22e9
Maria <fagundem@bc.edu>
2024-09-09 04:20:40 +0000

    commit_note

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to enhance Santander Bank's product recommendation system using machine learning techniques applied to a large dataset of customer behavior. By analyzing 1.5 years of historical data, including customer demographics and product ownership, we will develop a more personalized and effective recommendation model. The goal is to improve customer satisfaction, increase product adoption rates, and ultimately boost the bank's revenue through more targeted and relevant product suggestions.',
+    author='Maria Vieira and Aritra Ray',
+    license='MIT',
+)
